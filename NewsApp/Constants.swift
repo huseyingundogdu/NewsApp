8c858508edeb8fb342b411bed32fefd7efc54c9a
Huseyin <benhuseyingundogdu@gmail.com>
2023-12-19 14:19:50 +0100

    Create Constants file.

---
+++ NewsApp/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  NewsApp
+//
+//  Created by Huseyin on 16/12/2023.
+//
+
+import Foundation
+
+struct K {
+    static let APIKey = "fdde1a35e8724a1fa78d13ff63a9c1ef"
+    
+}
