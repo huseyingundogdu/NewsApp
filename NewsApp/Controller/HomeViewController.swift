534e61a3d8a80a965f1f5c645806e2a4de963ff6
Huseyin <benhuseyingundogdu@gmail.com>
2023-12-19 17:32:16 +0100

    Add getNews method.

--- NewsApp/Controller/HomeViewController.swift
+++ NewsApp/Controller/HomeViewController.swift
@@ -6,23 +6,43 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class HomeViewController: UIViewController {
 
     
     @IBOutlet weak var tableView: UITableView!
     
+    var articles: [Article]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.dataSource = self
+        tableView.delegate = self
+        registerTableViewCells()
         
+        getNews()
 
     }
     
     func getNews() {
-        
+        AF.request("https://newsapi.org/v2/top-headlines?country=us&apiKey=\(K.APIKey)").responseDecodable(of: News.self) { response in
+            
+            switch response.result {
+            case .success(let news):
+                self.articles = news.articles
+            case .failure(let error):
+                print("errorXXX: \(error)")
+            }
+            self.tableView.reloadData()
+        }
+    }
+    
+    func registerTableViewCells() {
+        let newsCell = UINib(nibName: "NewsTableViewCell", bundle: nil)
+        tableView.register(newsCell, forCellReuseIdentifier: "NewsCell")
     }
     
 }
@@ -32,16 +52,26 @@ extension HomeViewController: UITableViewDelegate {
 }
 
 extension HomeViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return articles?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .red
-        cell.textLabel?.text = "AAA"
-        cell.textLabel?.textColor = .black
-        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath) as! NewsTableViewCell
+            
+            if let article = articles?[indexPath.row] {
+                
+                cell.titleLabel.text = article.title
+                cell.descriptionLabel.text = article.description
+                
+                //TODO: cell.newsImage.image
+                cell.newsImage.image = UIImage(named: "placeholderImage")
+            
+            return cell
+        }
+        
+        return UITableViewCell()
     }
     
     
