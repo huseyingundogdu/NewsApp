7736df2018653a8b830febd63b89bc150ea4d3ee
Huseyin <benhuseyingundogdu@gmail.com>
2023-12-19 17:16:30 +0100

    Change publishedAt: Date to publishedAt: String.

--- NewsApp/Model/News.swift
+++ NewsApp/Model/News.swift
@@ -20,7 +20,7 @@ struct Article: Codable {
     let description: String?
     let url: String
     let urlToImage: String?
-    let publishedAt: Date
+    let publishedAt: String
     let content: String?
 }
 
