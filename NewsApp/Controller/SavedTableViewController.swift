2dff59b7cc3395c6ba756dd7bb9ceeffcdb99e15
Huseyin <benhuseyingundogdu@gmail.com>
2024-01-05 16:31:03 +0100

    .

--- NewsApp/Controller/SavedTableViewController.swift
+++ NewsApp/Controller/SavedTableViewController.swift
@@ -9,37 +9,29 @@ import UIKit
 
 class SavedTableViewController: UITableViewController {
 
+    var savedArticles = ["Article1", "Article2", "Article3", "Article4", "Article5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return savedArticles.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dummyCell", for: indexPath)
 
         // Configure the cell...
-
+        cell.textLabel?.text = savedArticles[indexPath.row]
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
